2166a75bb8df2c8a327a4c2708cd85d3901930a9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-08-14 11:25:01 -0500

    Added redux libraries

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <header className="App-header">
       <div>How's the water</div>
-      <div>Login</div>
+      <div>
+        <a href="https://testg301.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=65g3ii72skpsb08aggmq6a65fk&redirect_uri=http://localhost:3000/home">
+          Login
+        </a>
+      </div>
     </header>
   );
 }
