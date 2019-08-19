5cfa42a0d755678e6b6623dab0f4d005972f98fd
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-08-19 16:20:50 -0500

    Updated the buttons for facebook and google

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/App.css";
 import { Auth } from "aws-amplify";
+//import btn_google from "../assets/btn_google_signin.png";
+//import btn_facebook from "../assets/btn_facebook_signin.png";
 
 const LoginForm = () => {
   return (
@@ -8,14 +10,37 @@ const LoginForm = () => {
       <header className="App-header">
         <div>How's the water</div>
       </header>
-      <div>
-        <button onClick={() => Auth.federatedSignIn({ provider: "Facebook" })}>
-          Sign In with Facebook
-        </button>
-
-        <button onClick={() => Auth.federatedSignIn({ provider: "Google" })}>
-          Sign In with Google
-        </button>
+      <div className="body_content">
+        <div className="signin_content">
+          <div>
+            {/* <img
+              src={btn_facebook}
+              className="signin_btn_fb"
+              alt="facebook sign-in"
+              onClick={() => Auth.federatedSignIn({ provider: "Facebook" })}
+            /> */}
+            <button
+              className="loginBtn loginBtn--facebook"
+              onClick={() => Auth.federatedSignIn({ provider: "Facebook" })}
+            >
+              Login with facebook
+            </button>
+          </div>
+          <div>
+            {/* <img
+              src={btn_google}
+              className="signin_btn_g"
+              alt="google sing-in"
+              onClick={() => Auth.federatedSignIn({ provider: "Google" })}
+            /> */}
+            <button
+              className="loginBtn loginBtn--google"
+              onClick={() => Auth.federatedSignIn({ provider: "Google" })}
+            >
+              Login with Google
+            </button>
+          </div>
+        </div>
       </div>
     </fragment>
   );
